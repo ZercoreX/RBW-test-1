348b582af338c005798a4b8bd8b87e00fb1f9ba8
Zercore <119522892+Mohammed-Abdelnasser@users.noreply.github.com>
2024-07-15 15:39:50 +0300

    attempt to fix 1
    try1 of trying to fix jquery not working when hosting

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,9 @@ const links = document.querySelectorAll('.button-link');
 var activepage = document.querySelector('#home');
 
 links.forEach(link => {
-  console.log(link.id);
   if (link.id == pageMeta) {
     activepage.classList.remove("active");
     link.classList.add("active");
     activepage = link;
-    console.log(activepage);
   } 
 });
