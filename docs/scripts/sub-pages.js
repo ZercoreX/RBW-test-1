eb34880eec5edbc4304c2b86e273b37a3020a048
Zercore <119522892+ZercoreX@users.noreply.github.com>
2024-07-17 04:49:42 +0300

    added sub-pages feature

---
+++ docs/scripts/sub-pages.js
@@ -0,0 +1,24 @@
+const sublinks = document.querySelectorAll('.sub-link');
+var activesubpage;
+
+const pageContent = document.querySelector('#page-content');
+
+sublinks.forEach(
+  sublink => {
+    if (sublink.id == subPageMeta.content) {
+        activesubpage = sublink
+    }
+
+    sublink.addEventListener('click', function(event) {
+      event.preventDefault();
+      
+      activesubpage.classList.remove("active");
+      sublink.classList.add("active");
+      subPageMeta.setAttribute('content', sublink.id);
+      activesubpage = sublink;
+
+      $(pageContent).empty()
+      $(pageContent).load(`/docs/pages/${pageMeta.content}/${subPageMeta.content}.html`);
+      
+    });
+  });
